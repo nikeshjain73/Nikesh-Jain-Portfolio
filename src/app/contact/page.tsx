3923a0441ad525c1d4180d4555253b691d834d9d
Nikesh jain <jnikesh050@gmail.com>
2025-09-14 01:33:36 +0530

    post

--- src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="container py-4">
-      <h1>Contact</h1>
-      <p>Contact information from Nikesh Resume will appear here.</p>
-    </div>
-  );
-}
--- src/app/projects/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="container py-4">
-      <h1>Projects</h1>
-      <p>Project details from Nikesh Resume will appear here.</p>
-    </div>
-  );
-}
